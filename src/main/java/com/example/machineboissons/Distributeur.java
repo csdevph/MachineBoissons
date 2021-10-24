4512cc5aff664875a9480c0784092772bf767f26
csdevph <dev.ph@gmx.com>
2021-10-24 16:24:50 +0200

    Purge classe Distributeur

--- src/main/java/com/example/machineboissons/Distributeur.java
+++ src/main/java/com/example/machineboissons/Distributeur.java
@@ -5,35 +5,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Distributeur {
-    private int choix;
-
     public ArrayList<Boisson> getBoissons() {
         return boissons;
     }
 
     private final ArrayList<Boisson> boissons = new ArrayList<>();
-
-    private int saisirNumero() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Votre choix ?");
-        int nb = sc.nextInt();
-        return nb - 1;
-    }
-
-    void choisirBoisson() {
-        Boisson b = boissons.get(saisirNumero());
-        System.out.println("Votre choix : " + b.getNom());
-        System.out.println("Prix : " + b.getPrix());
-    }
-
-    void afficherBoissons() {
-        Boisson b;
-        System.out.println("\nListe des boissons :");
-        for (int i = 0; i < boissons.size(); i++) {
-            b = boissons.get(i);
-            System.out.println(i + 1 + " -> " + b.getNom() + ", " + b.getPrix() + " cents," + b.getTemp());
-        }
-    }
 
     private void ajouterBoissson(String ligne) {
         String[] data = ligne.split(";");
@@ -46,7 +22,6 @@ public class Distributeur {
         ));
     }
 
-    //todo voir Scanner p 197
     void chargerBoissons() throws IOException {
         String nomFichier;
         nomFichier = "src\\main\\java\\com\\example\\machineboissons\\data.txt";
@@ -55,7 +30,6 @@ public class Distributeur {
             while (s.hasNext()) {
                 ajouterBoissson(s.next());
             }
-            afficherBoissons();
         }
     }
 }
